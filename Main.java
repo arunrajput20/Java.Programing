59cf58cd0d0df4bb37e7351a1000051c48648f7f
arunrajput20 <143693745+arunrajput20@users.noreply.github.com>
2023-10-17 12:04:17 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,13 @@
- class Data
-{
-    private String name;
-    public void setter(String s){
-        name = s;
-    }
-    public String getter ()
-    {
-        System.out.println(name);
-        String s = name;
-        return s;
-    }
-}
+//try catch block
+
 public class Main{
-	public static void main(String[] args) {
-	    Data d= new Data();
-	    d.setter("java");
-	    System.out.println(d.getter());
-	}
+    public static void main(String[]args){
+        try{
+            int d= 5/0;
+            System.out.println("Rest of code in try block");
+        }
+        catch(ArithmeticException e){
+             System.out.println("ArithmeticException=>" + e.getMessage());
+        }
+    }
 }
